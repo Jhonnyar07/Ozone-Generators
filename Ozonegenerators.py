8f128d75e578d179cfa8fc355ba7b6ec37fbc28e
Jhonnyar07 <109990667+Jhonnyar07@users.noreply.github.com>
2024-01-19 15:05:55 +0100

    Update Ozonegenerators.py

--- Ozonegenerators.py
+++ Ozonegenerators.py
@@ -71,7 +71,7 @@ if option == "Ozono Agua":
             st.markdown(Pe)
         st.markdown("<h3 style='text-align: center;'>Producción requerida (g/h)</3>", unsafe_allow_html=True)
         Pr = (1.5*Pe)/(0.8*0.9)
-        st.markdown(Pr)
+        st.markdown(Pr.round(2))
         
 
 
