a15b39615f7ace6bdd2af33a55f0eeece397aa5f
Jhonnyar07 <109990667+Jhonnyar07@users.noreply.github.com>
2024-01-19 13:41:04 +0100

    Update Ozonegenerators.py

--- Ozonegenerators.py
+++ Ozonegenerators.py
@@ -22,18 +22,32 @@ st.image("https://i.imgur.com/fhOBqO5.jpg")
 option=st.selectbox('Tipo de instalación:', ('Ozono Ambiente', 'Ozono Agua'), index=None)
 
 if option == "Ozono Agua":
-    col1,col2,col3,col4 = st.columns(4)
+    col1,col2,col3 = st.columns(3)
     with col1:
-        Vr = st.number_input("Volumen del recipiente (Litros)", value=None)
+        st.write("Factor de seguridad por transferencia de masa")
+        st.write("0.9")
     with col2:
-        Qc = st.number_input("Caudal de consumo en producción (m3/h)", value=None)
+        st.write("Factor de seguridad por rendimiento de generación")
+        st.write("0.8")
     with col3:
-        Qr = st.number_input("Caudal de recirculación (m3/h)", value=None)
+        st.write("Factor de seguridad general de diseño")
+        st.write("1.5")
+        
+    
+    col4,col5,col6,col7 = st.columns(4)
     with col4:
+        Vr = st.number_input("Volumen del recipiente (Litros)", value=None)
+    with col5:
+        Qc = st.number_input("Caudal de consumo en producción (m3/h)", value=None)
+    with col6:
+        Qr = st.number_input("Caudal de recirculación (m3/h)", value=None)
+    with col7:
         C = st.number_input("Concentración objetivo de Ozono Disuelto (PPM)", value=None)
     calcule = st.button("Calcular",use_container_width=True)
     if calcule == True:
-        st.text("Hello World")
+        col8,col9,col10= st.columns(3)
+        with col8:
+            
 
 
 
