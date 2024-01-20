3f3348855e221fc9e4c5313d917586ea0ffed088
Jhonnyar07 <109990667+Jhonnyar07@users.noreply.github.com>
2024-01-20 11:56:48 +0100

    Update Ozonegenerators.py

--- Ozonegenerators.py
+++ Ozonegenerators.py
@@ -212,6 +212,21 @@ if option == "Ozono Agua":
             with col12:
                 st.image('https://i.imgur.com/j4Ujngv.jpg')
 
+        if Pr>4 and Pr<=6:
+            col11,col12 = st.columns(2)
+            with col11:
+                st.markdown("<h4 style='text-align: center;'>SP-5 30 Oxi</h4>", unsafe_allow_html=True)
+                with open("Ficha Técnica SP5 oxi.pdf", "rb") as file:
+                    btn = st.download_button(
+                    label="Ficha Técnica",
+                    data=file,
+                    file_name="Ficha Técnica SP5 oxi.pdf",
+                    mime="Doc/pdf",
+                    use_container_width=True
+                    )        
+            with col12:
+                st.image('https://i.imgur.com/j4Ujngv.jpg')
+
 
 st.write("----------------------------------------------------------------------------------------------------")
 
