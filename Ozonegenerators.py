7e257ed665356048491f1f06f949f9d758b54048
Jhonnyar07 <109990667+Jhonnyar07@users.noreply.github.com>
2025-10-12 20:03:35 +0200

    Update Ozonegenerators.py

--- Ozonegenerators.py
+++ Ozonegenerators.py
@@ -621,78 +621,18 @@ if option == "Ozono Aire":
     
     if optiona == None:
         SelectedQg=None
-    elif optiona == 'Z1250T':
+    elif optiona in ['Z1250T','Z3000T','Z6000T','Z10000T','Z20000T']:
         SelectedQg=518
-    elif optiona == 'Z3000T':
-        SelectedQg=518
-    elif optiona == 'Z6000T':
-        SelectedQg=518
-    elif optiona == 'Z10000T':
-        SelectedQg=518
-    elif optiona == 'Z20000T':
-        SelectedQg=518
-    elif optiona == 'ZHI1250':
+    elif optiona in ['ZHI1250','ZHI3000', 'ZHI6000','ZHI10000','ZHI300MG','ZHI500MG','ZHI1000MG','SP Mini 300mg','SP Mini 500mg','SP Mini 1g','SP Mini 2g']:
         SelectedQg=8
-    elif optiona == 'ZHI3000':
-        SelectedQg=8      
-    elif optiona == 'ZHI6000':
-        SelectedQg=8
-    elif optiona == 'ZHI10000':
-        SelectedQg=8
-    elif optiona == 'ZHI300MG':
-        SelectedQg=8
-    elif optiona == 'ZHI500MG':
-        SelectedQg=8
-    elif optiona == 'ZHI1000MG':
-        SelectedQg=8
-    elif optiona == 'Cañon 5':
+    elif optiona in ['Cañon 5','Cañon 12']:
         SelectedQg=3333
-    elif optiona == 'Cañon 12':
-        SelectedQg=3333
-    elif optiona == 'SP Mini 300mg':
-        SelectedQg=8
-    elif optiona == 'SP Mini 500mg':
-        SelectedQg=8
-    elif optiona == 'SP Mini 1g':
-        SelectedQg=8
-    elif optiona == 'SP Mini 2g':
-        SelectedQg=8
-    elif optiona == 'SP 300mg':
+    elif optiona in ['SP 300mg','SP 500mg','SP 1g','SP 2g','SP 4g','SP 8g','SP 10g','SP 15g']:
         SelectedQg=30
-    elif optiona == 'SP 500mg':
-        SelectedQg=30
-    elif optiona == 'SP 1g':
-        SelectedQg=30
-    elif optiona == 'SP 2g':
-        SelectedQg=30
-    elif optiona == 'SP 4g':
-        SelectedQg=30
-    elif optiona == 'SP 8g':
-        SelectedQg=30
-    elif optiona == 'SP 10g':
-        SelectedQg=30
-    elif optiona == 'SP 15g':
-        SelectedQg=30
-    elif optiona == 'SP5 20':
+    elif optiona in ['SP5 20','SP5 30','SP5 45','SP5 60','SP18','SP20A'] :
         SelectedQg=10
-    elif optiona == 'SP5 30':
-        SelectedQg=10
-    elif optiona == 'SP5 45':
-        SelectedQg=10
-    elif optiona == 'SP5 60':
-        SelectedQg=10
-    elif optiona == 'SP21 20':
+    elif optiona in ['SP21 20','SP21 40','SP5 Oxi 20','SP5 Oxi 30']:
         SelectedQg=1.5
-    elif optiona == 'SP21 40':
-        SelectedQg=1.5
-    elif optiona == 'SP5 Oxi 20':
-        SelectedQg=1.5
-    elif optiona == 'SP5 Oxi 30':
-        SelectedQg=1.5
-    elif optiona == 'SP18':
-        SelectedQg=10
-    elif optiona == 'SP20A':
-        SelectedQg=510
 
 
     st.divider()
