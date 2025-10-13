4f39f406b01ded45faf78a11b1e59f12ca28f37d
Jhonnyar07 <109990667+Jhonnyar07@users.noreply.github.com>
2025-10-13 16:47:10 +0200

    Update Ozonegenerators.py

--- Ozonegenerators.py
+++ Ozonegenerators.py
@@ -606,11 +606,11 @@ else:
                 with col19:
                     if optiona != None and optiona != 'Calculo sin generador asignado':
                         Qg = SelectedQg
-                        st.markdown(f"Caudal de salida del gas: {Qg} L/min")
+                        st.markdown(f"Caudal de salida del gas: {Qg} L/min", width='content')
                 with col20:
                     if optiona != None and optiona != 'Calculo sin generador asignado':
                         Pr = SelectedPr
-                        st.markdown(f"Producción seleccionada: {Pr} gr/h")
+                        st.markdown(f"Producción seleccionada: {Pr} gr/h", width='content')
 
         if optiona != None:
             Vei = st.number_input("Volumen del espacio a tratar", value=None)
